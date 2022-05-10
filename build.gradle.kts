0f94c118362083eb7b8dd789a4b9855068f79d7c
Javier de la Pena <javier.pfayos@gmail.com>
2022-05-10 08:28:26 +0200

    Adding swagger

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("io.springfox:springfox-boot-starter:3.0.0")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("nio:nio:1.0.4")
